fbf25d7d0a400f212ea6d90d558c020f353dfb57
mlnlmsl <melamsal555@gmail.com>
2019-10-16 07:07:30 +0545

    restart and score added

--- car-race/js/Game.js
+++ car-race/js/Game.js
@@ -1,19 +1,28 @@
 function Game() {
     this.gameElement = document.getElementById('game-container');
+    this.scoreElement = document.createElement("span");
+    this.reload = document.createElement("button");
+    this.gameOver = document.createElement("div");
     this.carElement = null;
     this.obstacleElement = null;
     this.obstacles = [];
     this.road = null;
     this.GAME_ANIMATION_FRAME = 24;
     this.startGame = null;
+    this.score = 0;
 
     this.init = function () {
-        this.gameElement.style.backgroundColor = 'green';
         this.gameElement.style.margin = '0px auto';
         this.gameElement.style.height = '600px';
         this.gameElement.style.width = '600px';
         this.gameElement.style.position = 'relative';
         this.gameElement.style.overflow = 'hidden';
+        this.scoreElement.style.position = 'absolute';
+        this.scoreElement.style.top = '20px';
+        this.scoreElement.style.left = '20px';
+        this.scoreElement.style.fontSize = '20px';
+        this.scoreElement.style.zIndex = '100';
+        this.gameElement.appendChild(this.scoreElement);
         this.addElement();
         this.startGame = setInterval(this.move.bind(this), this.GAME_ANIMATION_FRAME);
         // setInterval(this.addObstacle.bind(this), this.GAME_ANIMATION_FRAME * 20);
@@ -32,11 +41,13 @@ function Game() {
         var rem_arr = [];
         this.road.moveRoad();
         // this.obstacleElement.move();
+        this.scoreElement.textContent = this.score;
         this.addObstacle();
         for (let i = 0; i < this.obstacles.length; i++) {
             this.obstacles[i].move();
             if (this.obstacles[i].yPositionTop >= 600) {
                 this.obstacles[i].remove();
+                this.score++;
                 rem_arr.push(i);
             }
         }
@@ -63,11 +74,28 @@ function Game() {
         }
     }
 
+    this.restart = function () {
+        location.reload();
+    }
+
     this.carCollision = function () {
         for (let i = 0; i < this.obstacles.length; ++i) {
             if (this.carElement.currentLane == this.obstacles[i].lane) {
                 if (this.obstacles[i].yPositionTop + 100 >= this.carElement.topPos) {
                     clearInterval(this.startGame);
+                    this.reload.textContent = "restart";
+                    this.gameOver.textContent = "Game Over";
+                    this.reload.style.zIndex = '100';
+                    this.gameOver.style.zIndex = '100';
+                    this.reload.style.position = 'absolute';
+                    this.gameOver.style.position = 'absolute';
+                    this.gameOver.style.top = '30px';
+                    this.reload.style.top = '50px';
+                    this.gameOver.style.left = '40%';
+                    this.reload.style.left = '40%';
+                    this.gameElement.appendChild(this.gameOver);
+                    this.gameElement.appendChild(this.reload);
+                    this.reload.onclick = this.restart;
 
                 }
             }
--- car-race/js/index.js
+++ car-race/js/index.js
@@ -1,19 +1,22 @@
-var game = new Game();
-game.init();
-
-window.addEventListener("keydown", event => {
-    event.preventDefault();
-    switch (event.keyCode) {
-        case 32: //key code of space
-            // game.move();
-            break;
-        case 37: //key code left
-            game.moveCarLeft();
-            break;
-        case 39: // key code right
-            game.moveCarRight();
-            break;
-        default:
-            break;
-    }
-});
+var start = document.getElementById("start");
+function startGame() {
+    start.style.display = 'none';
+    var game = new Game();
+    game.init();
+    window.addEventListener("keydown", event => {
+        event.preventDefault();
+        switch (event.keyCode) {
+            case 32: //key code of space
+                // game.move();
+                break;
+            case 37: //key code left
+                game.moveCarLeft();
+                break;
+            case 39: // key code right
+                game.moveCarRight();
+                break;
+            default:
+                break;
+        }
+    });
+}
